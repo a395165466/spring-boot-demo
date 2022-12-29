5d3c5fbe61cd7e49da7eeefd24a48a5012841e34
zhangguoqing <zhanggq_1989@126.com>
2022-12-29 16:30:06 +0800

    + spring-boot 增加module

--- spring-boot-demo-starter/src/main/java/com/github/zhangguqing/Application.java
+++ spring-boot-demo-starter/src/main/java/com/github/zhangguqing/Application.java
@@ -1,4 +1,4 @@
-package com.company.project;
+package com.github.zhangguqing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
 
