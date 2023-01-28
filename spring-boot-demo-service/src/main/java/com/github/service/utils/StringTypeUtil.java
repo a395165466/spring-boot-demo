49453d9ab282330fe3b7f3a6e283d15191124dd0
zhangguoqing <zhanggq_1989@126.com>
2023-01-28 16:52:37 +0800

    + spring-boot 增加string相关的工具方法

--- spring-boot-demo-service/src/main/java/com/github/service/utils/StringTypeUtil.java
+++ spring-boot-demo-service/src/main/java/com/github/service/utils/StringTypeUtil.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 
 /**
  * 字符串类型的工具类
+ * https://blog.csdn.net/weixin_43272286/article/details/114240541?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-1-114240541-blog-126677151.pc_relevant_aa2&spm=1001.2101.3001.4242.2&utm_relevant_index=4
+ * https://blog.csdn.net/u012060033/article/details/89326485
+ * https://blog.csdn.net/RRros/article/details/124825123
  */
 public class StringTypeUtil {
     /**
