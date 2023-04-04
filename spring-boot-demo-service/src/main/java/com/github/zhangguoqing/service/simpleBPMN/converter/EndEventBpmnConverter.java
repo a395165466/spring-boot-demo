3dd9289965faa9b513bc5b11a6e0dc7743864c85
zhangguoqing <zhanggq_1989@126.com>
2023-04-04 20:02:02 +0800

    + spring-boot 增加bpmnConverter

--- spring-boot-demo-service/src/main/java/com/github/zhangguoqing/service/simpleBPMN/converter/EndEventBpmnConverter.java
+++ spring-boot-demo-service/src/main/java/com/github/zhangguoqing/service/simpleBPMN/converter/EndEventBpmnConverter.java
@@ -1,6 +1,7 @@
 package com.github.zhangguoqing.service.simpleBPMN.converter;
 
 import com.github.zhangguoqing.service.simpleBPMN.model.BpmnParseModel;
+import com.github.zhangguoqing.service.simpleBPMN.model.element.EndEventElement;
 import com.github.zhangguoqing.service.simpleBPMN.model.element.ProcessElement;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,13 @@ import javax.xml.stream.XMLStreamReader;
 public class EndEventBpmnConverter implements BaseBpmnConverter {
     @Override
     public void converter(XMLStreamReader xtr, BpmnParseModel model, ProcessElement processElement) {
+        String id = xtr.getAttributeValue(null, "id");
+        String name = xtr.getAttributeValue(null, "name");
 
+        EndEventElement endEventElement = new EndEventElement();
+        endEventElement.setId(id);
+        endEventElement.setName(name);
+
+        processElement.getFlowItemElements().add(endEventElement);
     }
 }
--- spring-boot-demo-service/src/main/java/com/github/zhangguoqing/service/simpleBPMN/converter/ProcessBpmnConverter.java
+++ spring-boot-demo-service/src/main/java/com/github/zhangguoqing/service/simpleBPMN/converter/ProcessBpmnConverter.java
@@ -2,14 +2,22 @@ package com.github.zhangguoqing.service.simpleBPMN.converter;
 
 import com.github.zhangguoqing.service.simpleBPMN.model.BpmnParseModel;
 import com.github.zhangguoqing.service.simpleBPMN.model.element.ProcessElement;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Component;
 
 import javax.xml.stream.XMLStreamReader;
+import java.util.ArrayList;
 
 @Component
 public class ProcessBpmnConverter implements BaseBpmnConverter {
     @Override
     public void converter(XMLStreamReader xtr, BpmnParseModel model, ProcessElement processElement) {
+        String id = xtr.getAttributeValue(null, "id");
+        String name = xtr.getAttributeValue(null, "name");
 
+        processElement.setId(id);
+        processElement.setName(name);
+
+        model.setProcessElementList(Lists.newArrayList(processElement));
     }
 }
